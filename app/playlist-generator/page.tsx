78443df374b87d972ed3ed2681ee8ab46a0c9df2
imad selka <104644824+imadselka@users.noreply.github.com>
2024-06-19 19:04:16 +0100

    feat: Add Spotify playlist generator page and header component
    The code changes include:
    - Added a new file `app/playlist-generator/page.tsx` which contains the implementation of the Spotify playlist generator page.
    - Added a new file `components/header.tsx` which contains the implementation of the header component.
    - Updated the `app/layout.tsx` file to set the title and description metadata for the Spotify playlist generator.
    
    These changes introduce the functionality to generate a playlist based on the user's top tracks from Spotify.

---
+++ app/playlist-generator/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>You are connected !</div>;
+};
+
+export default page;
---
+++ components/header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const REDIRECT_URI = "http://localhost:3000/playlist-generator";
+  const RESPONSE_TYPE = "token";
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold text-center mt-10">
+          Spotify Playlist Generator
+        </h1>
+        <p className="text-center mt-2">
+          Login with your Spotify account to generate a playlist based on your
+          top tracks
+        </p>
+      </div>
+      <div className="flex justify-center items-center mt-2">
+        <Link
+          href={`${AUTH_ENDPOINT}?client_id=${process.env.client_id}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          <Button>Login with Spotify</Button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Header;
